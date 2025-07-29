98f045af239fe0b906b497e411fef70063d29bcc
CBertrams-RevoData <cas.bertrams@revodata.nl>
2025-07-29 13:56:28 +0200

    Created function to mask api response to minimize tokens passed to llm
    Added function mask_api_response to utils. Api's call this function together with the correct mask to prune Databricks' response.

---
+++ src/databricks_mcp/api/response_masks.py
@@ -0,0 +1,23 @@
+get_table_details_mask = {
+    "name": {},
+    "schema_name": {},
+    "catalog_name": {},
+    "comment": {},
+    "columns": 
+            {
+                "name": {},
+                "type_text": {},
+                "comment": {}
+            }
+}
+
+get_jobs_mask = {
+        "job_id": {}, 
+        "settings": 
+                {
+                    "name": {},
+                    "description": {}
+                },
+        "tables": []
+        }
+
